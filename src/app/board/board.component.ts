56ed9333e2bcfd536d6a9a9ca18b0f1ed63632be
Unni <69468073+hash-dotcom@users.noreply.github.com>
2020-11-02 19:31:30 +0530

    init

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -38,5 +38,34 @@ export class BoardComponent implements OnInit {
       this.squares.splice(idx,1,this.player);
       this.xisnext=!this.xisnext;
     }
+    this.winner=this.calculatethewinner();
   }
+
+  calculatethewinner()
+{
+  const lines=[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i=0;i<lines.length;i++)
+  {
+    const [a,b,c]=lines[i];
+    if(
+      this.squares[a] &&
+      this.squares[a]===this.squares[b] &&
+      this.squares[a]===this.squares[c]
+     )
+     {
+       return this.squares[a];
+     }
+  }
+   return null;
+}
 }
